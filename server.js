846f452e97cf02df9127baa0ca6a64476ead4cdf
ukr11 <udaykr119@gmail.com>
2020-11-15 22:39:50 +0530

    add login POST route

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body.parser');
+app.use(bodyParser.json());
 const mockUserData=[
 {name:'mark'},
 {name:'jill'}
@@ -32,6 +34,24 @@ app.get('/users/:id',function(req,res){
 	})
 })
 
+app.post('/login',function(req,res){
+	const username=req.body.username;
+	const password=req.body.password;
+	const mockUsername="billyTheKid";
+	const mockPassword="superSecret";
+	if (username===mockUsername && password===mockPassword){
+	res.json({
+		success: true,
+		message: 'password and username match!',
+		token: 'encrypted token goes here'
+	})
+	} else {
+	res.json({
+		success: false,
+		message: 'password and username do not match'
+	})
+	}
+})
 
 
 
